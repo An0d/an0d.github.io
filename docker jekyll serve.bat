809e63f8709f89147f724d50b7ba59b509c52f5b
An0d <viaene.gregory@gmail.com>
2019-11-15 17:00:21 +0100

    Update docker jekyll serve.bat

--- docker jekyll serve.bat
+++ docker jekyll serve.bat
@@ -1,1 +1,1 @@
-docker run --rm --volume="%cd%:/srv/jekyll" --volume="jekyll-vendor-bundle:/usr/local/bundle" -it -p 4000:4000 --env JEKYLL_ENV=development jekyll/jekyll:4.0 jekyll serve --force_polling --livereload
+docker run --rm --volume="%cd%:/srv/jekyll" --volume="jekyll-vendor-bundle:/usr/local/bundle" -it -p 4000:4000 -p 35729:35729 --env JEKYLL_ENV=development jekyll/jekyll:4.0 jekyll serve --force_polling --livereload
